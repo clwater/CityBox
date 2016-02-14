b3177dc7a9d3edee0b187c41a73fbd2111211053
clwater <gengzhibo@gengzhibo.net>
2016-02-14 20:24:05 +0800

    20160214——3

--- CityBox/app/src/main/java/com/uit/uit2013/ui/activity/LifeDangKouActivity.java
+++ CityBox/app/src/main/java/com/uit/uit2013/ui/activity/LifeDangKouActivity.java
@@ -2,25 +2,23 @@ package com.uit.uit2013.ui.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.twotoasters.jazzylistview.JazzyListView;
 import com.twotoasters.jazzylistview.effects.HelixEffect;
 import com.uit.uit2013.R;
 import com.uit.uit2013.model.DangKou;
+import com.uit.uit2013.model.Restaurant;
 import com.uit.uit2013.utils.db.DKDateCtrl;
+import com.uit.uit2013.utils.db.ResDateCtrl;
 import com.uit.uit2013.utils.network.DangKouNewWork;
 
 
@@ -64,7 +62,7 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
     }
 
     private void createlist() {
-       life_dangkou_listview = (JazzyListView)findViewById(R.id.life_dangkou_listview);
+        life_dangkou_listview = (JazzyListView)findViewById(R.id.life_dangkou_listview);
         life_dangkou_listview.setTransitionEffect( new HelixEffect());
 
         try {
@@ -72,13 +70,12 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
         }catch (Exception exceptione){}
 
         data = getData();
+        adapter = new SimpleAdapter(this,data,R.layout.item_dangkou,
+                new String[]{"name","price","sub","number","add"},
+                new int[]{R.id.itme_dk_name,R.id.itme_dk_price , R.id.item_dk_sub , R.id.item_dk_number , R.id.item_dk_add});
 
-        OrderingAdapter myadapter = new OrderingAdapter(getBaseContext(), data);
-
-
-        life_dangkou_listview.setAdapter(myadapter);
-
-
+        life_dangkou_listview.setAdapter(adapter);
+        //life_res_listview.setOnItemClickListener(this);
     }
 
     private List<Map<String, Object>> getData() {
@@ -88,7 +85,7 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
             map.put("name", ""+dangkou.get(i).getName().toString());
             map.put("price", "¥" + dangkou.get(i).getPrice().toString());
             map.put("sub","-");
-            map.put("shuliang","0");
+            map.put("number","0");
             map.put("add","+");
             list.add(map);
         }
@@ -149,117 +146,4 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
        // Toast.makeText(LifeDangKouActivity.this , "更新完成,请重新进入." ,Toast.LENGTH_SHORT).show();
         //this.finish();
     }
-
-
-    public class OrderingAdapter extends BaseAdapter {
-
-        private List<Map<String, Object>> data;
-        private LayoutInflater layoutInflater;
-        private Context context;
-        private int shuliang[];
-        private Zujian dk = new Zujian();
-
-
-        public OrderingAdapter(Context context, List<Map<String, Object>> data) {
-            this.context = context;
-            this.data = data;
-            this.layoutInflater = LayoutInflater.from(context);
-
-            shuliang = new int[data.size()];
-            for (int i = 0; i < data.size(); i++) {
-                shuliang[i] = 0;
-            }
-
-        }
-
-        /**
-         * 组件集合，对应list.xml中的控件
-         *
-         * @author Administrator
-         */
-        public final class Zujian {
-            public TextView name;
-            public TextView price;
-            public TextView number;
-            public Button sub;
-            public Button add;
-        }
-
-        @Override
-        public int getCount() {
-            return data.size();
-        }
-
-        /**
-         * 获得某一位置的数据
-         */
-        @Override
-        public Object getItem(int position) {
-            return data.get(position);
-        }
-
-        /**
-         * 获得唯一标识
-         */
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-
-            final int selectID = position;
-            if (convertView == null) {
-
-                //获得组件，实例化组件
-                convertView = layoutInflater.inflate(R.layout.item_dangkou, null);
-                dk.name = (TextView) convertView.findViewById(R.id.itme_dk_name);
-                dk.price = (TextView) convertView.findViewById(R.id.itme_dk_price);
-                dk.sub = (Button) convertView.findViewById(R.id.item_dk_sub);
-                dk.add = (Button) convertView.findViewById(R.id.item_dk_add);
-                dk.number = (TextView) convertView.findViewById(R.id.item_dk_number);
-                convertView.setTag(dk);
-            } else {
-                dk = (Zujian) convertView.getTag();
-            }
-            //绑定数据
-
-            dk.name.setText((String) data.get(position).get("name"));
-            dk.price.setText((String) data.get(position).get("price"));
-            dk.number.setText("123");
-            dk.sub.setText("-");
-            dk.sub.setTag(position);
-            dk.add.setTag(position);
-
-            final Zujian finalHolder = dk;
-            dk.sub.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.d("=-=", "sub: " + position + "   shuling :" + shuliang[position]);
-                    shuliang[position]--;
-                    
-                    finalHolder.number.setText("aaaa");
-                    Log.d("=-=", "number: " +  "" + finalHolder.number.getText().toString());
-
-
-                }
-            });
-            dk.add.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.d("=-=", "add: " + position + "   shuling :" + shuliang[position]);
-                    shuliang[position]++;
-                    Log.d("=-=", "number: " +  "" + finalHolder.number.getText().toString());
-
-                }
-            });
-
-            return convertView;
-        }
-
-
-    }
-
-
 }
