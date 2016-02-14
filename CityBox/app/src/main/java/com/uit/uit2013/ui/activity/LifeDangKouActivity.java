b03e071a47492763d66de70ed14643f2219c7ef4
clwater <gengzhibo@gengzhibo.net>
2016-02-14 17:40:09 +0800

    第一版本
    上一个好用这个不知道怎么不好用  换个思路看那可能

--- CityBox/app/src/main/java/com/uit/uit2013/ui/activity/LifeDangKouActivity.java
+++ CityBox/app/src/main/java/com/uit/uit2013/ui/activity/LifeDangKouActivity.java
@@ -1,13 +1,22 @@
 package com.uit.uit2013.ui.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -62,20 +71,36 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
     }
 
     private void createlist() {
-        life_dangkou_listview = (JazzyListView)findViewById(R.id.life_dangkou_listview);
-        life_dangkou_listview.setTransitionEffect( new HelixEffect());
+//        life_dangkou_listview = (JazzyListView)findViewById(R.id.life_dangkou_listview);
+//        life_dangkou_listview.setTransitionEffect( new HelixEffect());
+//
+//        try {
+//            dangkou = DKDateCtrl.QueryRes(this ,dangkouid_s);
+//        }catch (Exception exceptione){}
+//
+//        data = getData();
+//        adapter = new SimpleAdapter(this,data,R.layout.item_dangkou,
+//                new String[]{"name","price","sub","number","add"},
+//                new int[]{R.id.itme_dk_name,R.id.itme_dk_price , R.id.item_dk_sub , R.id.item_dk_number , R.id.item_dk_add});
+//
+//        life_dangkou_listview.setAdapter(adapter);
 
-        try {
+
+        //life_res_listview.setOnItemClickListener(this);
+
+
+       try {
             dangkou = DKDateCtrl.QueryRes(this ,dangkouid_s);
         }catch (Exception exceptione){}
 
-        data = getData();
-        adapter = new SimpleAdapter(this,data,R.layout.item_dangkou,
-                new String[]{"name","price","sub","number","add"},
-                new int[]{R.id.itme_dk_name,R.id.itme_dk_price , R.id.item_dk_sub , R.id.item_dk_number , R.id.item_dk_add});
 
+        data = getData();
+        life_dangkou_listview = (JazzyListView)findViewById(R.id.life_dangkou_listview);
+        //life_dangkou_listview.setTransitionEffect( new HelixEffect());
+
+
+        MyAdapter adapter = new MyAdapter(getBaseContext());
         life_dangkou_listview.setAdapter(adapter);
-        //life_res_listview.setOnItemClickListener(this);
     }
 
     private List<Map<String, Object>> getData() {
@@ -145,5 +170,80 @@ public class LifeDangKouActivity  extends Activity implements View.OnClickListen
         createlist();
        // Toast.makeText(LifeDangKouActivity.this , "更新完成,请重新进入." ,Toast.LENGTH_SHORT).show();
         //this.finish();
+    }
+
+
+
+
+    public class MyAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        public MyAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        public int getCount() {
+            return data.size();
+        }
+        public Object getItem(int position) {
+            return null;
+        }
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+        //****************************************final方法
+//注意原本getView方法中的int position变量是非final的，现在改为final
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+
+
+                convertView = mInflater.inflate(R.layout.item_dangkou, null);
+
+                holder.name = (TextView)convertView.findViewById(R.id.itme_dk_name);
+                holder.price = (TextView)convertView.findViewById(R.id.itme_dk_price);
+                holder.number = (TextView)convertView.findViewById(R.id.item_dk_number);
+
+                holder.sub = (Button)convertView.findViewById(R.id.item_dk_sub);
+                holder.add = (Button)convertView.findViewById(R.id.item_dk_add);
+                convertView.setTag(holder);
+            }else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            holder.name.setText((String)data.get(position).get("name"));
+            holder.price.setText((String)data.get(position).get("price"));
+            holder.number.setText((String)data.get(position).get("number"));
+            holder.add.setTag(position);
+            holder.sub.setTag(position);
+            //给Button添加单击事件  添加Button之后ListView将失去焦点  需要的直接把Button的焦点去掉
+            holder.add.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    showInfo(position);
+                   // Toast.makeText(this , "=-= ; jian position: " + position , Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //holder.viewBtn.setOnClickListener(MyListener(position));
+
+            return convertView;
+        }
+    }
+
+    public final class ViewHolder {
+        TextView name ,price , number;
+        Button sub , add;
+
+    }
+
+    public void showInfo(int position){
+        new AlertDialog.Builder(this)
+                .setTitle("详情"+position)
+                .show();
     }
 }
